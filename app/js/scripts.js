896f0a347dac31d56b13fe3c0a189515951c956d
xkored <xkored@gmail.com>
2019-07-19 20:38:48 +0400

    created marks in mp

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -10,12 +10,23 @@ $(function() {
 		items: 1,
 		loop: true,
 		dots: true,
+		margin: 20,
 		smartSpeed: 900,
 		autoplay: true,
 		autoplayHoverPause: true,
-		// autoHeight: true,
-		dotsContainer: '.sel__in-dots',
+		
+		
 		navText: ["<img src='img/slider__arrow_prev-black.png'>", "<img src='img/slider__arrow_next-black.png'>"],
+		responsive : {
+			0   : {
+					nav: false,
+					autoHeight: true,
+			},
+			1200 : {
+					nav: true,
+					dotsContainer: '.sel__in-dots',
+			}
+		}
 	});
 	//scroll by click 'next btn'
 	$('.sel_next').on('click', function(e) {
@@ -42,13 +53,8 @@ $(function() {
 		navText: ["<img src='img/slider__arrow_prev-gray.png'>", "<img src='img/slider__arrow_next-gray.png'>"],
 		responsive : {
 			0   : {
-					items: 1
-			},
-			380 : {
-					items: 1
-			},
-			480 : {
-					items: 1
+					items: 1,
+					// autoHeight: true,
 			},
 			768 : {
 					items: 3
@@ -88,47 +94,103 @@ $(function() {
 		e.preventDefault();
 		$(this).parent().toggleClass('active');
 	})
+	$(document).on('click', function (e) {
+		var container = $('.header__cab');
+
+		// if the target of the click isn't the container nor a descendant of the container
+		if (!container.is(e.target) && container.has(e.target).length === 0) 
+		{
+				container.removeClass('active')
+		}
+	});
 	//hamb
 	$(".hamburger__checkbox").change(function() {
 		$('.header__menu').toggleClass('active');
 		$('.hamburger__wrp').toggleClass('active');
+		if ( $(window).width() < 1200 ) {
+			$('body').toggleClass('fixed');
+		}
 	});
 	//wow
 	$(function() {
 		new WOW().init();
 	});
 	// Tooltips
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
+	if ( $(window).width() > 1200 ) {
+		$(document).ready(function(){
+			$('[data-toggle="tooltip"]').tooltip();
+		});
+	};
+	$('.pop-popup').hide();
+	$('.pop__item-rate').on('click', function(){
+		$(this).parents('.pop__item-wrp').toggleClass('hover').find('.pop-popup').slideToggle();
 	});
-	$("[data-toggle=popover]").popover({
-		html : true,
-		trigger: "hover focus",
-		// delay: {
-		// 		show: 100,
-		// 		hide: 100
-		// },
-		show: function () {
-				$(this).fadeIn('slow');
-		},
-		content: function() {
-			var content = $(this).attr("data-popover-content");
-			return $(content).children(".popover-body").html();
-		}
+	$('.pop-popup__close').on('click', function(){
+		$(this).parents('.pop__item-wrp').toggleClass('hover').find('.pop-popup').slideToggle();
 	});
+	// $(document).on('click', function (e) {
+	// 	var container2 = $('.pop-popup');
+	// 	// if the target of the click isn't the container nor a descendant of the container
+	// 	if (!container2.is(e.target) && container2.has(e.target).length === 0) 
+	// 	{
+	// 			container2.hide();
+	// 			$('.pop__item-wrp2').removeClass('hover');
+	// 	}
+	// });
+	if ( $(window).width() < 1200 ) {
+		$('.pop__list').removeClass('row').addClass('owl-carousel');
+		$('.pop__item-wrp').removeClass('col-xl-4 col-sm-6');
+
+		$('.pop__list').owlCarousel({
+			nav: false,
+			// items: 2,
+			loop: true,
+			smartSpeed: 700,
+			margin: 20,
+			dots: true,
+			responsive : {
+				0   : {
+						items: 1
+				},
+				380 : {
+						items: 1
+				},
+				480 : {
+						items: 1
+				},
+				768 : {
+						items: 2
+				},
+				1040 : {
+						items: 3
+				}
+			}
+	 });
+	};
+	$('.pop__item-rate').hover
 	//main offer
-	$('.banner__bar-btn').on('click',function(e){
-		e.preventDefault();
-		$('.banner-filter__wrp').slideToggle();
-		$(this).toggleClass('active').parents('.banner__offer').toggleClass('filter-open');
-	});
-	$('.banner-filter__top-btn').on('click',function(e){
-		e.preventDefault();
-		$('.banner-filter__wrp').slideToggle();
-		$('.banner__bar-btn').removeClass('active');
-		$(this).parents('.banner__offer').toggleClass('filter-open');
-	});
-	
+	if ( $(window).width() > 1200 ) {
+		$('.banner__bar-btn').on('click',function(e){
+			e.preventDefault();
+			$('.banner-filter__wrp').slideToggle();
+			$(this).toggleClass('active').parents('.banner__offer').toggleClass('filter-open');
+		});
+		$('.banner-filter__top-btn').on('click',function(e){
+			e.preventDefault();
+			$('.banner-filter__wrp').slideToggle();
+			$('.banner__bar-btn').removeClass('active');
+			$(this).parents('.banner__offer').toggleClass('filter-open');
+		});
+	};
+	if ( $(window).width() < 1200 ) {
+		$('.banner-filter__top-btn').on('click', function () {
+			$.fancybox.close();
+		});
+		$('.banner-filter__act').hide();
+		$('.banner-filter input').on('input', function(){
+			$('.banner-filter__act').slideDown();
+		});
+	};
 	//jquery slider
 	$(".banner-filter__slider").slider({
 		min: 0,
@@ -138,13 +200,14 @@ $(function() {
 		stop: function(event, ui) {
 		$("input.banner-filter__input_min").val($(".banner-filter__slider").slider("values",0));
 		$("input.banner-filter__input_max").val($(".banner-filter__slider").slider("values",1));
+			$('.banner-filter__act').slideDown();
 		},
 		slide: function(event, ui){
 		$("input.banner-filter__input_min").val($(".banner-filter__slider").slider("values",0));
 		$("input.banner-filter__input_max").val($(".banner-filter__slider").slider("values",1));
+			$('.banner-filter__act').slideDown();
 		}
 	});
-
 	$("input.banner-filter__input_min").on('input', function(){
 		var value1=$("input.banner-filter__input_min").val();
 		var value2=$("input.banner-filter__input_max").val();
@@ -155,8 +218,6 @@ $(function() {
 		}
 		$(".banner-filter__slider").slider("values",0,value1);
 	});
-
-		
 	$("input.banner-filter__input_max").on( 'input', function() {
 		var value1=$("input.banner-filter__input_min").val();
 		var value2=$("input.banner-filter__input_max").val();
@@ -171,10 +232,10 @@ $(function() {
 	});
 	//accordeon
 	if ( $(window).width() > 768 ) {
-		$('.banner-filter__item').removeClass('acc__item');
+		$('.banner-filter__item').parent().removeClass('acc__item-wrp');
 	};
-	$('.acc__item .acc__cnt').hide();
-	$('.acc__item .acc__btn').on('click', function () {
-		$(this).toggleClass('active').parent('.acc__item').siblings('.acc__cnt').slideToggle();
+	$('.acc__item-wrp .acc__cnt').hide();
+	$('.acc__item-wrp .acc__btn').on('click', function () {
+		$(this).toggleClass('active').parents('.acc__item-wrp').find('.acc__cnt').slideToggle();
 	});
 });
